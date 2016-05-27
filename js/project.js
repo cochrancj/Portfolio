20ff1a6eab30c5067322509bd33ece2b17631add
Carolyn Cochran <cochran.cj@gmail.com>
2016-05-27 17:00:07 -0400

    works on shit

--- js/project.js
+++ js/project.js
@@ -1,72 +1,46 @@
 $(document).ready(function(){
 
-  $(".nav-button-about").click(function() {
+  $(".about").click(function() {
     // $("#toggle_about").toggle(1000);
     // $(".jumbotron").toggleClass("large");
       if ($(this).data('name') == 'show'){
         $("#toggle_about").animate({
-            width: '0%'
+            // width: '0%'
           }).hide()
           $(".jumbotron").animate({
-            width: '100%'
+            // width: '100%'
           });
           $(this).data('name', 'hide')
       } else {
         $("#toggle_about").animate({
-            width: '19%'
+            // width: '19%'
           }).show()
           $(".jumbotron").animate({
-            width: '80%'
+            // width: '80%'
           });
           $(this).data('name', 'show')
       }
   });
 
-  $(".nav-button-cv").click(function() {
+  $(".cv").click(function() {
     $("#toggle_cv").toggle(1000);
     $(".jumbotron").toggleClass("extralarge");
   });
 
-  $(".nav-button-design").click(function() {
+  $(".design").click(function() {
     $("#toggle_design").toggle(1000);
     $(".jumbotron").toggleClass("extralarge");
   });
 
-  $(".nav-button-photography").click(function() {
+  $(".photography").click(function() {
     $("#toggle_photography").toggle(1000);
     $(".jumbotron").toggleClass("large");
   });
 
-  $(".nav-button-writing").click(function() {
+  $(".writing").click(function() {
     $("#toggle_writing").toggle(1000);
     $(".jumbotron").toggleClass("large");
   });
 
-
-
-
-  // FIX JQUERY SO THAT WHEN YOU CLICK SOMETHING ELSE,
-  // THE DIV THAT IS EXPANDED COLLAPSES SO THE
-  // NEW DIV CAN EXPAND. THIS DOES NOT WORK, BUT I
-  // THINK IS CLOSE TO WORKING vv
-
-  //   $(".jumbotron").click(function (e) {
-  //       if(e.target.className == "large" || "extralarge") {
-  //            e.stopPropagation();
-  //       }
-  //       else {
-  //           $("*").removeClass("large" || "extralarge");
-  //       }
-  //   });
-
-  // THIS ALSO DOESN'T WORK vv
-
-  //   $(".nav-button-about").click(function() {
-  //   $("#toggle_about").toggle(1000);
-  //   $(".jumbotron").toggleClass("large");
-  //   if ($(e.target).is(".jumbotron") === false) {
-  //     $(".jumbotron").removeClass("large");
-  //   }
-  // });
 
 });
