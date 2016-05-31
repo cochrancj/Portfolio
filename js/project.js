975227bcd67d7c8d87dee224cfb786c449beabdf
Carolyn Cochran <cochran.cj@gmail.com>
2016-05-30 20:34:43 -0400

    works on flexbox stuff

--- js/project.js
+++ js/project.js
@@ -1,35 +1,18 @@
 $(document).ready(function(){
 
   $(".about").click(function() {
-    // $("#toggle_about").toggle(1000);
-    // $(".jumbotron").toggleClass("large");
-      if ($(this).data('name') == 'show'){
-        $("#toggle_about").animate({
-            // width: '0%'
-          }).hide()
-          $(".jumbotron").animate({
-            // width: '100%'
-          });
-          $(this).data('name', 'hide')
-      } else {
-        $("#toggle_about").animate({
-            // width: '19%'
-          }).show()
-          $(".jumbotron").animate({
-            // width: '80%'
-          });
-          $(this).data('name', 'show')
-      }
+    $("#toggle_about").toggle(1000);
+    $(".jumbotron").toggleClass("large");
   });
 
   $(".cv").click(function() {
     $("#toggle_cv").toggle(1000);
-    $(".jumbotron").toggleClass("extralarge");
+    $(".jumbotron").toggleClass("large");
   });
 
   $(".design").click(function() {
     $("#toggle_design").toggle(1000);
-    $(".jumbotron").toggleClass("extralarge");
+    $(".jumbotron").toggleClass("large");
   });
 
   $(".photography").click(function() {
